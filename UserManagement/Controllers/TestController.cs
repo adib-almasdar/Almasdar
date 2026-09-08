283d83dce47631073b1072dae26a32b2694ab3ca
Vineet Kumar <vineet.kumar@acuvate.com>
2026-09-08 13:56:42 +0530

    adding test controller and put the logic to get environment variable

---
+++ UserManagement/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/<TestController>
+        [HttpGet("GetEnvironementVariable")]
+        public IActionResult GetEnvironementVariable()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+                  ?? "Development";
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: false)
+                .Build();
+            var env = configuration["Environment"];
+            return Ok(configuration["Environment"]);
+        }
+    }
+}
